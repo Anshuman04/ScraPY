483886b4c248ec543e7d471151d8a437951f7bab
manav9498 <manav.gandhi0904@gmail.com>
2025-10-04 14:29:01 -0500

    Resolved  comments

--- practice/manav/homework_calculator_Manav.py
+++ practice/manav/homework_calculator_Manav.py
@@ -1,22 +1,28 @@
 name = input("Enter your name: ")
 print(f"Welcome {name} to the calculator world!.\nWhat do you want to do?")
 
-while True:
+while True: # this is always true
     choice = input("Enter your choice:\n1. or Addition\n2. or Subtraction\n3. or Multiplication\n4. or Division\n5. or Exit ")
+    
+    if choice == "5." or choice == "Exit":
+        print(f"Thank you for using the calculator")
+        break
+    
     num_1 = float(input("Enter first number: "))
     num_2 = float(input("Enter second number: "))
     
     if choice == "1." or choice == "Addition":
         print(f"Addition of {num_1} and {num_2} is {num_1 + num_2}")
+        
     elif choice == "2." or choice == "Subtraction":
         print(f"Subtraction of {num_1} and {num_2} is {num_1-num_2}")
+        continue
     elif choice == "3." or choice == "Multiplication":
         print(f"Multiplication of {num_1} and {num_2} is {num_1*num_2}")
+        continue
     elif choice == "4." or choice == "Division":
         if num_2 == 0:
             print(f"Division by zero is not allowed")
+            continue
         else:
-            print(f"Division of {num_1} and {num_2} is {num_1/num_2}")
-    else:
-        print(f"Thank you for using the calculator")
-        break
+            print(f"Division of {num_1} and {num_2} gives quotient {num_1//num_2} and remainder {num_1 % num_2}")
