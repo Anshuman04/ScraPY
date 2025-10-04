6776c006298d1cd5f4c4e752c896673737aa0bd3
root2mishra <rutumishra115@gmail.com>
2025-10-04 14:57:34 -0400

    Rutu Class Notes

--- practice/root2/calculator.py
+++ practice/root2/calculator.py
@@ -1,9 +1,11 @@
 name = input("Enter your name: ")
 print(f"Welcome {name} to the calculator world!")
-option = input("Enter your option: ")
-print(f"You entered {option}")
-while option != "Exit" and option != "exit":
+while True:
     print(f"What do you want to do?\n 1. Addition\n 2. Subtraction\n 3. Multiplication\n 4. Division\n 5. Exit")
+    option = input("Enter your option: ")
+    print(f"You entered {option}")
+    if option == "Exit" or option == "5" or option == "exit":
+        break
     var1 = int(input("Enter your first number: "))
     var2 = int(input("Enter your second number: "))
     if option == "Addition" or option == "1" or option == "add" or option == "Add" or option == "addition":
@@ -13,7 +15,8 @@ while option != "Exit" and option != "exit":
     elif option == "Multiplication" or option == "3" or option == "mul" or option == "Mul" or option == "multiplication":
         print(f"Multiplication of {var1} with {var2} is {var1 * var2}")
     elif option == "Division" or option == "4" or option == "div" or option == "Div" or option == "division":
-        print(f"Dividing {var1} by {var2} gives {var1 // var2} as quotient and {var1 % var2} as remainder")
-    else:
-        print("Exit")
-    option = input("Enter your option: ")
+        if var2 == 0:
+            print ("Division by zero is not allowed")
+        else:
+            print(f"Dividing {var1} by {var2} gives {var1 // var2} as quotient and {var1 % var2} as remainder")
+    
